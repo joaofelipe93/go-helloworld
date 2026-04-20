7e58d8370146205c6657e1d54849519407f3de7d
João Felipe <joaofelipe.beta@gmail.com>
2026-04-20 19:16:53 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joaofelipe93/go-helloworld
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	nome := "João"
+	idade := 33
+	learn := true
+	
+	fmt.Println("Nome: ", nome)
+	fmt.Println("Idade: ", idade)
+	fmt.Println("Estuda GO?: ", learn)
+}
